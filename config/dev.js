d964acd7e6762fd8c96abe853c9939a09dc13920
koren levi <koren012349@gmail.com>
2021-12-19 21:36:49 +0200

    koren

--- config/dev.js
+++ config/dev.js
@@ -1,12 +1,6 @@
 module.exports = {
   // 'dbURL': 'mongodb://localhost:27017',
-  // devServer: {
-  //   disableHostCheck: true,
-  //   host: '192.168.1.169',
-  //   port: 3000,
-  //   public: '0.0.0.0:3000',
-  //   https: false
-  // },
+ 
   // publicPath: "/",
 
   'dbURL': 'mongodb+srv://koren:12345@cluster0.asnuq.mongodb.net/trelloxDB?retryWrites=true&w=majority',
