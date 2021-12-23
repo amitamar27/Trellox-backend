9f5ef6ff542ebbcb65f2e8ef041acd6c1de940b8
koren levi <koren012349@gmail.com>
2021-12-23 20:46:52 +0200

    new port

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/**', (req, res) => {
 
 
 const logger = require('./services/logger.service')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3035
 http.listen(port, () => {
     logger.info('Server is running on port: ' + port)
 })
